ce41ab8ab0da8355d20c91c22488b05e7d4f25c3
Venicius Feitosa <feitosa.veniciusf@gmail.com>
2023-09-30 15:13:07 -0300

    Classe ReceitaCulinaria

--- src/main/java/com/persist/receitas/App.java
+++ src/main/java/com/persist/receitas/App.java
@@ -1,13 +1,30 @@
 package com.persist.receitas;
 
-/**
- * Hello world!
- *
- */
+import java.util.ArrayList;
+import java.util.List;
+
+// import ReceitaCulinaria;
+class salvarCSV{
+    public static void salvar(){
+        
+    }
+}
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+        
+        // ReceitaCulinaria r1 = new ReceitaCulinaria(1, "Mousse de Limão", "Sobremesa");
+        // List<Ingrediente> ingredientes;
+        // ingredientes = new ArrayList<>();
+        // ingredientes.add(new Ingrediente("Suco de Limão", "2", "Colheres"));
+        // ingredientes.add(new Ingrediente("Leite Condensado", "1", "Caixa"));
+        // r1.adicionarIngrediente(ingredientes);
+        // System.out.println(r1);
+        // r1.listarIngredientes();
+        // r1.adicionarInstrucoes();
+        
     }
 }
